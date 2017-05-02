b9d57837223cab418faf707bd3650dc73f7fb2cd
richardbadman <rg236@brighton.ac.uk>
2017-05-02 17:42:44 +0100

    Updated start scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+mv /var/www/html/Shift-API/.env.example /var/www/html/Shift-API/.env
+cd /var/www/html/Shift-API && php artisan key:generate
 cd /var/www/html/Shift-API && screen -S Shift-API -d -m npm run dev
 cd /var/www/html/Shift-API && screen -S Shift-API -d -m php -S 0.0.0.0:8080 -t public/
 cd /var/www/html/Shift-API && screen -S Shift-API -d -m php artisan migrate
