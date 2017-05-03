0d716961ac9f7f6911cfe4a3580acd84d8d743a8
richardbadman <rg236@brighton.ac.uk>
2017-05-03 19:44:03 +0100

    Re-added auth to Shift controller

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -8,10 +8,10 @@ use App\Employee;
 
 class ShiftController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
